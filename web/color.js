61a6d96f84ecc855332a8e51146f4a45c8b573e6
DAEWOOYANG0310 <ydw8088@naver.com>
2024-06-06 11:34:00 +0900

    add

--- web/color.js
+++ web/color.js
@@ -1,34 +1,39 @@
-var body = {
-  backgroundsetColor:function(color){
-  document.querySelector('body').style.backgroundColor = color;
+let Link = {
+  setcolor:function(color){
+let alist = document.querySelectorAll('a');
+let i = 0;
+while(i < alist.length){
+alist[i].style.color = color;
+i = i + 1;
+}
+}
+}
+
+
+let Body = {
+setcolor:function(color){
+  document.querySelector('body').style.color = color;
 },
-  setColor:function(color){
-    document.querySelector('body').style.color = color;
-  }
-} 
-var links = {
-    setColor:function(color){
-    var alist = document.querySelectorAll('a');
-     var i = 0;
-     while(i < alist.length){
-     alist[i].style.color = color;
-     i = i + 1;
+setbackgroundcolor:function(color){
+  document.querySelector('body').style.backgroundColor = color;
 }
-  }
+}
+function nightdayhandler(self){
+if(document.querySelector('#night_day').value === 'night'){
+ Body.setbackgroundcolor('black');
+ Body.setcolor('white');
+ self.value = 'day'
+ Link.setcolor('powderblue')
+}else{
+Body.setbackgroundcolor('white');
+Body.setcolor('black');
+self.value = 'night'
+Link.setcolor('blue')
+
+}
 }
 
 
-function nightdayHandler(self){
-  var target=document.querySelector('body')
-  if(self.value === 'night'){
-     body.backgroundsetColor('black');
-     self.value = 'day'; 
-     body.setColor('white'); 
-     links.setColor('white');  
-  }else{
-    body.backgroundsetColor('white');
-    self.value = 'night'; 
-    body.setColor('black'); 
-    links.setColor('black');
-    } 
-  }
+
+
+
